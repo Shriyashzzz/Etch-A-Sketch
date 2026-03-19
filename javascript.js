4c845e6c7b82385f51a062a43ee4288e2e376d1c
Shriyashzzz <ghimires1@my.smccd.edu>
2026-03-18 22:55:16 -0700

    add css style, add reset button.

--- javascript.js
+++ javascript.js
@@ -3,18 +3,40 @@
 makeGrid()
 const mainContainer = document.querySelector(".mainContainer")
 
+const btnContainer = document.createElement("div");
+btnContainer.classList.add("btnContainer");
 
 const askUserSquaresBtn = document.createElement('button')
 askUserSquaresBtn.textContent = "Custom Grid"
 askUserSquaresBtn.classList.add("Button")
+btnContainer.appendChild(askUserSquaresBtn)
 
 askUserSquaresBtn.addEventListener('click',userPromptGrid)
-mainContainer.before(askUserSquaresBtn);
+
+
+mainContainer.before(btnContainer);
+
+let resetBtn = document.createElement("button");
+resetBtn.textContent = "Reset Grid"
+resetBtn.classList.add('Button');
+btnContainer.appendChild(resetBtn);
+let gridSize ;
+resetBtn.addEventListener('click',()=>{
+    makeGrid(+gridSize)
+    appendSquareVals()
+})
+
 
 let mainPage = document.querySelector("#page")
-let gridSize = document.createElement('p');
-gridSize.textContent = '16 X 16';
-mainPage.appendChild(gridSize);
+let gridSizeText = document.createElement('p');
+gridSizeText.classList.add("para")
+gridSizeText.textContent = '16 X 16';
+gridSizeText.style.fontSize = "20px"
+mainContainer.before(gridSizeText);
+
+
+
+
 
 function userPromptGrid(){
     usergivenSquares = prompt("How many number of squares per side for the new grid?", 16);
@@ -23,14 +45,10 @@ function userPromptGrid(){
         return
     }
     else if(usergivenSquares <=100 && usergivenSquares>0){ 
-        gridSize.textContent = `${usergivenSquares} X ${usergivenSquares}`;
+        gridSizeText.textContent = `${usergivenSquares} X ${usergivenSquares}`;
+        gridSize = usergivenSquares;
         makeGrid(usergivenSquares);
-        const squares = document.querySelectorAll(".square");
-        squares.forEach((square) =>{
-            square.style.height = `${(672 / usergivenSquares)}px`
-            square.style.width = `${(672 / usergivenSquares)}px`
-        })
-        console.log(squares[1].style.height)
+        appendSquareVals()
         
 
     }else{
@@ -38,18 +56,24 @@ function userPromptGrid(){
     }
 }
 
-function makeGrid(givenSquare = 16){
+function makeGrid(givenSquare =16){
     const mainContainer = document.querySelector(".mainContainer")
     mainContainer.replaceChildren();
     for(let i =0; i <(givenSquare*givenSquare);i++){
     const square = document.createElement("div")
     square.classList.add("square")
     square.addEventListener('mouseenter', () =>{  
-        square.style.backgroundColor = "black";
+        square.style.backgroundColor = "#36494E";
     })
     mainContainer.appendChild(square)
     }
 }
 
 
-
+function appendSquareVals(){
+    const squares = document.querySelectorAll(".square");
+        squares.forEach((square) =>{
+            square.style.height = `${(672 / usergivenSquares)}px`
+            square.style.width = `${(672 / usergivenSquares)}px`
+        })
+}
